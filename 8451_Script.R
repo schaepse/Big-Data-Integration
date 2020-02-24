f7ce9456e9fbf2ae1f85ffa79ea458a640ead650
schaepse <schaepse@mail.uc.edu>
2020-02-24 13:28:16 -0500

    Updating the Results

--- 8451_Script.R
+++ 8451_Script.R
@@ -4,12 +4,23 @@ library(tidyverse)
 install.packages("tibble")
 library(tibble)
 
+remove.packages("rlang")
 setwd("~/8451_Carbo-Loading/Carbo-Loading CSV")
 
 #Read datasets#
 transaction <- read_csv("transactions.csv")
 causal <- read_csv("causal_lookup.csv")
 #I might want to join these by store and upc for these too#
-transaction_group <- transaction %>%
-  select(upc, dollar_sales, store) %>%
-  group_by(upc, store)
+transaction_filter <- transaction %>%
+  filter(week > 42)
+
+causal_edit <- causal %>%
+  dplyr::select(upc, store, week, feature_desc, display_desc)
+
+remove.packages("dplyr")
+install.packages("dplyr")
+library(dplyr)
+install.packages("pillar")
+library(pillar)
+
+update.packages()
